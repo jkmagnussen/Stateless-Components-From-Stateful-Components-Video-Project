96710c413ceadc8bb34ac2770796a4b3a52b4b29
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-05-08 22:28:26 +0100

    first commit

---
+++ src/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export class Menu extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    const text = e.target.value
+    this.props.chooseVideo(text);
+  }
+
+  render() {
+    return (
+      <form onClick={this.handleChange}>
+        <input type="radio" name="src" value="fast" /> fast
+        <input type="radio" name="src" value="slow" /> slow
+        <input type="radio" name="src" value="cute" /> cute
+        <input type="radio" name="src" value="eek" /> eek
+      </form>
+    );
+  }
+}
